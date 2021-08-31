6f4e06b01d0aa91bbcde172987680848185138b4
Theseregrets <rishitashaw49@gmail.com>
2021-08-31 17:57:17 +0530

    smart contracts integrated

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,7 @@
+//Import Contractconst DStorage = artifacts.require("DStorage");
+const DStorage = artifacts.require("DStorage");
+
+module.exports = function (deployer) {
+  //Deploy Contract
+  deployer.deploy(DStorage);
+};
