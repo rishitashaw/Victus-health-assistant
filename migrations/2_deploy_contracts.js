349d70a7f116da628b36461e028ed99a0f7e57fc
Theseregrets <rishitashaw49@gmail.com>
2021-09-11 08:10:13 +0530

    few changes made

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,11 +2,8 @@
 const DStorage = artifacts.require("DStorage");
 const HStorage = artifacts.require("HStorage");
 
-
 module.exports = function (deployer) {
   //Deploy Contract
   deployer.deploy(DStorage);
   deployer.deploy(HStorage);
-
 };
-
